a521559b4eb9099a445edabb673ba9f0d535bc66
will <willsmith12289@gmail.com>
2016-04-05 16:16:08 -0400

    express.js server

---
+++ expressApp/server.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+	app		= express();
+
+app.get('/', function(req, res){
+	res.send('hello world');
+});
+//.listen(3000);
+
+
+
+//Alternative method to appending like above
+
+app.listen(3000, function(){
+	console.log("listening on port 3000");
+});
